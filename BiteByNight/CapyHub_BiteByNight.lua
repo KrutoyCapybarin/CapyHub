7c88f17989a4b5a9b0d9d202481b05c4bf33134f
KrutoyCapybarin <KrutoyCapybarin@gmail.com>
2026-05-13 06:01:35 +0600

    dsadsa

---
+++ BiteByNight/CapyHub_BiteByNight.lua
@@ -0,0 +1,4 @@
+if lattemode == true then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KrutoyCapybarin/CapyHub/refs/heads/main/BiteByNight/CapyHub_BiteByNight_LatteEdition.lua"))()
+else
+  print("CapyHub: Internal mode is not out yet.")
