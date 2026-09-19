f63d6f7699a07479f8468d4767f4cbf774e1644c
KrutoyCapybarin <KrutoyCapybarin@gmail.com>
2026-09-19 13:15:26 +0500

    dxdddd

--- BiteByNight/CapyHub_BiteByNight.lua
+++ BiteByNight/CapyHub_BiteByNight.lua
@@ -1,5 +1,10 @@
 if lattemode == true then
+  print("CapyHub: Launching BiteByNight Latte mode.")
   loadstring(game:HttpGet("https://raw.githubusercontent.com/KrutoyCapybarin/CapyHub/refs/heads/main/BiteByNight/CapyHub_BiteByNight_LatteEdition.lua"))()
-else
-  error("CapyHub: Internal mode is not out yet.")
 end
+if getexecutorname and getexecutorname() == "CapyWare" then 
+  warn("CapyHub: Error caused trying to run BiteByNight using CapyWare. There is no version for CapyWare YET. If you belive this is wrong, report on CapyWare X CapyHub discord server. Link is in your clipboard.")
+  
+end
+
+  error("CapyHub: Internal mode is not out yet.")
